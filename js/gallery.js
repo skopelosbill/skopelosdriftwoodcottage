4376751c0d10d154c3ad14fb017dfbb979de0f90
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-08-16 15:47:41 +0300

    16 Aug [4]

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image, index) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+            
+
+            getLatestOpenedImg = index + 1;
+
+            let fullImgContainer = document.body;
+            let newImgWindow = document.createElement("div");
+            fullImgContainer.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "img/" + setNewImgUrl);
+            
+        }
+    });
+}
