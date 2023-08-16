5e676f9cca78c0bfdfff7c4b04659fe776d20074
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-08-16 16:16:58 +0300

    16 Aug [5]

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,7 @@ if(galleryImages) {
         image.onclick = function() {
             let getElementCss = window.getComputedStyle(image);
             let getFullImgUrl = getElementCss.getPropertyValue("background-image");
-            let getImgUrlPos = getFullImgUrl.split("");
+            let getImgUrlPos = getFullImgUrl.split("/img/thumbs");
             let setNewImgUrl = getImgUrlPos[1].replace('")', '');
             
 
