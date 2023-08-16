b0f7c27a695055874849889356ed00050bbf1ebb
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-08-16 09:24:05 +0300

    16 Aug [1]

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav").style.width = "250px";   
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+}
+
+function openLarge(pic) {
+    document.getElementById("pic").style.width = "70%"
+}
