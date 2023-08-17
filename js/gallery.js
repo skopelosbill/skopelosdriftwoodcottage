a8e2de0329c8da3443f4b8e3977ff7a7d8e5b7e9
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-08-17 12:20:25 +0300

    17 Aug [1]

--- js/gallery.js
+++ js/gallery.js
@@ -22,7 +22,60 @@ if(galleryImages) {
             let newImg = document.createElement("img");
             newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "img/" + setNewImgUrl);
+            newImg.setAttribute("id", "current-img");
+
+            newImg.onload = function() {
+                let imgWidth = this.width;
+                let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+                let newNextBtn = document.createElement("a");
+                let btnNextText = document.createTextNode("Next");
+                newNextBtn.appendChild(btnNextText);
+                fullImgContainer.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class", "img-btn-next");
+                newNextBtn.setAttribute("onclick", "changeImg(1)");
+                newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+
+                let newPrevBtn = document.createElement("a");
+                let btnPrevText = document.createTextNode("Prev");
+                newPrevBtn.appendChild(btnPrevText);
+                fullImgContainer.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class", "img-btn-prev");
+                newPrevBtn.setAttribute("onclick", "changeImg(0)");
+                newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
+            }
             
         }
     });
+}
+function closeImg() {
+    document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
+}
+
+function changeImg(changeDir) {
+    document.querySelector("#current-img").remove();
+
+    let getImgWindow = document.querySelector(".img-window");
+    let newImg = document.createElement("img");
+    getImgWindow.appendChild(newImg);
+   
+    let calcNewImg;
+    if (changeDir === 1) {
+        calcNewImg = getLatestOpenedImg + 1;
+        if (calcNewImg > galleryImages.length) {
+            calcNewImg = 1;
+        }
+    } 
+    else if ((changeDir === 0)) {
+        calcNewImg = getLatestOpenedImg - 1;
+        if (calcNewImg < 1) {
+            calcNewImg =  galleryImages.length;
+        }
+    }
+    
+        newImg.setAttribute("src", "img/img" + calcNewImg + ".jpg");
+        newImg.setAttribute("id", "current-img");
+    
+   
 }
